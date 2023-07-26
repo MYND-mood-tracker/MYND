cc8262b8852eeec196c4dc06c36d2c0abc975e61
Ahnaf Khan <ahnafkhan3@gmail.com>
2023-07-26 16:10:47 -0700

    finished first page of appplication

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,41 +1,49 @@
-// LandingPage.jsx
 import React, { useState } from 'react';
 import '../styles.css';
 
 const LandingPage = () => {
-  const [number, setNumber] = useState(''); 
+  const [number, setNumber] = useState('');
+  const [text, setText] = useState('');
 
   const handleNumberChange = (increment) => {
-    
     setNumber((prevNumber) => {
-      if (prevNumber === '' && increment === -1) return 5; 
-      if (prevNumber === '' && increment === 1) return 1; 
+      if (prevNumber === '' && increment === -1) return '5';
+      if (prevNumber === '' && increment === 1) return '1';
       const newNumber = Number(prevNumber) - increment;
-      return Math.min(Math.max(newNumber, 1), 5); 
+      const clampedNumber = Math.min(Math.max(newNumber, 1), 5).toString();
+      setText(textMessages[clampedNumber]); // Update text based on clampedNumber
+      return clampedNumber;
     });
+  }; 
+
+  const textMessages = {
+    '1': "Terrible",
+    '2': "Bad",
+    '3': "Not bad",
+    '4': "Good",
+    '5': "Fantastic",
   };
 
   return (
     <div className="container">
       <div className="message-wrapper">
-        <div className="message">Hey, how are you feeling today?</div>
+        <div className="message">Hey! How was your day?</div>
       </div>
-      <div className="number-box-wrapper">
-        <div className={`number-box ${number === '3' ? 'text-not-bad' : ''}`}>
-          {number}
-          <div className="arrow-buttons">
-            <span className="arrow-button" onClick={() => handleNumberChange(-1)}>
-              &#x25B2; {/* Up arrow */}
-            </span>
-            <span className="arrow-button" onClick={() => handleNumberChange(1)}>
-              &#x25BC; {/* Down arrow */}
-            </span>
-          </div>
+      <div className={`number-box number-${number}`}> 
+        <div className="number-text">{number}</div>
+        <div className="arrow-buttons">
+          <span className="arrow-button" onClick={() => handleNumberChange(-1)}>
+            &#x25B2;
+          </span>
+          <span className="arrow-button" onClick={() => handleNumberChange(1)}>
+            &#x25BC;
+          </span>
         </div>
       </div>
+      <div className="text-message">{text}</div>
       <button className="submit-button">Submit</button>
     </div>
   );
-}
+};
 
 export default LandingPage;
