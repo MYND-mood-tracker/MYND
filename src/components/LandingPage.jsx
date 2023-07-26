bc514aae9f1832def074a22902aa54505428f495
Ahnaf Khan <ahnafkhan3@gmail.com>
2023-07-26 11:43:17 -0700

    fixed up and down arrow functionality

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
     setNumber((prevNumber) => {
       if (prevNumber === '' && increment === -1) return 5; 
       if (prevNumber === '' && increment === 1) return 1; 
-      const newNumber = Number(prevNumber) + increment;
+      const newNumber = Number(prevNumber) - increment;
       return Math.min(Math.max(newNumber, 1), 5); 
     });
   };
@@ -21,20 +21,21 @@ const LandingPage = () => {
         <div className="message">Hey, how are you feeling today?</div>
       </div>
       <div className="number-box-wrapper">
-      <div className={`number-box ${number === '3' ? 'text-not-bad' : ''}`}>{number}</div>
-      </div>
-      <div>
-        <span className="arrow-button" onClick={() => handleNumberChange(-1)}>
-          &#x25BC; 
-        </span>
-        <span className="arrow-button" onClick={() => handleNumberChange(1)}>
-          &#x25B2;
-        </span>
+        <div className={`number-box ${number === '3' ? 'text-not-bad' : ''}`}>
+          {number}
+          <div className="arrow-buttons">
+            <span className="arrow-button" onClick={() => handleNumberChange(-1)}>
+              &#x25B2; {/* Up arrow */}
+            </span>
+            <span className="arrow-button" onClick={() => handleNumberChange(1)}>
+              &#x25BC; {/* Down arrow */}
+            </span>
+          </div>
+        </div>
       </div>
       <button className="submit-button">Submit</button>
     </div>
   );
-};
-  
+}
 
 export default LandingPage;
